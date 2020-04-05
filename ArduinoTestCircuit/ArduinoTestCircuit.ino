178944d6222d3ef0a1fd39b352bbfc69c1430d0b
Chris Page <chris_pagey100@hotmail.com>
2020-04-05 10:09:34 +0100

    Test circuit with button mimicking wheel rotation working

--- ArduinoTestCircuit/ArduinoTestCircuit.ino
+++ ArduinoTestCircuit/ArduinoTestCircuit.ino
@@ -1,4 +1,5 @@
-// the setup routine runs once when you press reset:
+#include <PCD8544.h>
+ PCD8544 lcd;
 
 float wheelDiam = 0.000622;
 bool sensorFound = false;
@@ -7,6 +8,7 @@ long rotations = 0;
 const int buttonPin = 2;
 const int ledPin =  13; 
 
+// the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(38400);
@@ -14,6 +16,7 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+  lcd.begin(84, 48);
 }
 
 // the loop routine runs over and over again forever:
@@ -34,6 +37,13 @@ void updateMileageTest(int buttonState) {
     Serial.print(" km ");
     Serial.print(rotations);
     Serial.println(" rotations");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(dtostrf(totalMileage, 5, 4, str));
+    lcd.print(" km");
+    lcd.setCursor(0, 1);
+    lcd.print(rotations);
+    lcd.print(" rotations");
   }
 
   if (buttonState == LOW) {
